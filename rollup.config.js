c0a30e752c1c64d798dc64e2f24810f48b44f6ab
Tomas Carnecky <tomc@caurea.org>
2020-02-26 19:21:43 +0100

    Rename example/ to docs/

--- rollup.config.js
+++ rollup.config.js
@@ -3,5 +3,5 @@ import nodeResolve from "rollup-plugin-node-resolve";
 export default {
   input: "lib/example.js",
   plugins: [nodeResolve({ jsnext: true, main: true })],
-  output: { file: "example/app.js", format: "iife" }
+  output: { file: "docs/app.js", format: "iife" }
 };
